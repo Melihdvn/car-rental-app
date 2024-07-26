b43fb5f461eb2524ec483dc4118920b2ca4b9d57
Melihdvn <melihdvn@gmail.com>
2024-07-26 13:37:07 +0300

    Update database

--- car-rental/database/schema.sql
+++ car-rental/database/schema.sql
@@ -1,29 +1,116 @@
+--
+-- File generated with SQLiteStudio v3.4.4 on Fri Jul 26 13:31:37 2024
+--
+-- Text encoding used: System
+--
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: additional_services
-CREATE TABLE "additional_services" ("id" integer primary key autoincrement not null, "name" varchar not null, "price" numeric not null, "created_at" datetime, "updated_at" datetime);
+CREATE TABLE additional_services (
+    additional_service_id INTEGER  PRIMARY KEY AUTOINCREMENT
+                                   NOT NULL,
+    name                  VARCHAR  NOT NULL,
+    price                 NUMERIC  NOT NULL,
+    created_at            DATETIME,
+    updated_at            DATETIME
+);
+
 
 -- Table: migrations
-CREATE TABLE "migrations" ("id" integer primary key autoincrement not null, "migration" varchar not null, "batch" integer not null);
+CREATE TABLE migrations (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT
+                      NOT NULL,
+    migration VARCHAR NOT NULL,
+    batch     INTEGER NOT NULL
+);
+
 
 -- Table: reservation_services
-CREATE TABLE reservation_services (id integer PRIMARY KEY AUTOINCREMENT NOT NULL, reservation_id integer NOT NULL, service_id integer NOT NULL, created_at datetime, updated_at datetime, FOREIGN KEY (reservation_id) REFERENCES reservations (reservation_id) ON DELETE CASCADE, FOREIGN KEY (service_id) REFERENCES additional_services (id) ON DELETE CASCADE);
+CREATE TABLE reservation_services (
+    rs_id                 INTEGER  PRIMARY KEY AUTOINCREMENT
+                                   NOT NULL,
+    reservation_id        INTEGER  NOT NULL,
+    additional_service_id INTEGER  NOT NULL
+                                   REFERENCES additional_services (additional_service_id),
+    created_at            DATETIME,
+    updated_at            DATETIME,
+    FOREIGN KEY (
+        reservation_id
+    )
+    REFERENCES reservations (reservation_id) ON DELETE CASCADE,
+    FOREIGN KEY (
+        additional_service_id
+    )
+    REFERENCES additional_services (additional_service_id) ON DELETE CASCADE
+);
+
 
 -- Table: reservations
-CREATE TABLE reservations (reservation_id integer PRIMARY KEY AUTOINCREMENT NOT NULL, is_rented tinyint(1), user_id integer NOT NULL, vehicle_id integer NOT NULL, start_date date NOT NULL, end_date date NOT NULL, total_price numeric NOT NULL, created_at datetime, updated_at datetime);
+CREATE TABLE reservations (
+    reservation_id INTEGER     PRIMARY KEY AUTOINCREMENT
+                               NOT NULL,
+    is_rented      TINYINT (1),
+    user_id        INTEGER     NOT NULL,
+    vehicle_id     INTEGER     NOT NULL,
+    start_date     DATE        NOT NULL,
+    end_date       DATE        NOT NULL,
+    total_price    NUMERIC     NOT NULL,
+    created_at     DATETIME,
+    updated_at     DATETIME
+);
+
 
 -- Table: users
-CREATE TABLE users (user_id integer PRIMARY KEY AUTOINCREMENT NOT NULL, name varchar, email varchar NOT NULL, password varchar NOT NULL, is_active tinyint (1) NOT NULL DEFAULT (1), remember_token varchar, created_at datetime, updated_at datetime);
+CREATE TABLE users (
+    user_id        INTEGER     PRIMARY KEY AUTOINCREMENT
+                               NOT NULL,
+    name           VARCHAR,
+    email          VARCHAR     NOT NULL,
+    password       VARCHAR     NOT NULL,
+    is_active      TINYINT (1) NOT NULL
+                               DEFAULT (1),
+    remember_token VARCHAR,
+    created_at     DATETIME,
+    updated_at     DATETIME
+);
+
 
 -- Table: vehicles
-CREATE TABLE vehicles (vehicle_id integer PRIMARY KEY AUTOINCREMENT NOT NULL, make varchar NOT NULL, model varchar NOT NULL, fuel varchar NOT NULL, transmission varchar NOT NULL, year integer NOT NULL, kilometers INTEGER, is_active tinyint (1) NOT NULL DEFAULT '1', daily_rate numeric NOT NULL, created_at datetime, updated_at datetime);
+CREATE TABLE vehicles (
+    vehicle_id   INTEGER     PRIMARY KEY AUTOINCREMENT
+                             NOT NULL,
+    make         VARCHAR     NOT NULL,
+    model        VARCHAR     NOT NULL,
+    fuel         VARCHAR     NOT NULL,
+    transmission VARCHAR     NOT NULL,
+    year         INTEGER     NOT NULL,
+    kilometers   INTEGER,
+    is_active    TINYINT (1) NOT NULL
+                             DEFAULT '1',
+    daily_rate   NUMERIC     NOT NULL,
+    created_at   DATETIME,
+    updated_at   DATETIME
+);
+
 
 -- Table: verification_codes
-CREATE TABLE verification_codes (vc_id integer PRIMARY KEY AUTOINCREMENT NOT NULL, email varchar NOT NULL, code varchar NOT NULL, type varchar, created_at datetime, updated_at datetime);
+CREATE TABLE verification_codes (
+    vc_id      INTEGER  PRIMARY KEY AUTOINCREMENT
+                        NOT NULL,
+    email      VARCHAR  NOT NULL,
+    code       VARCHAR  NOT NULL,
+    type       VARCHAR,
+    created_at DATETIME,
+    updated_at DATETIME
+);
+
 
 -- Index: users_email_unique
-CREATE UNIQUE INDEX users_email_unique ON users ("email");
+CREATE UNIQUE INDEX users_email_unique ON users (
+    "email"
+);
+
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
